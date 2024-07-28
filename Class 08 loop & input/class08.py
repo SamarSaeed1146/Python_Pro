b7105c4cd42c49f42416948b4fdb9b40ad3b8ea2
SamarSaeed1146 <samarsaeed374@gmail.com>
2024-07-28 20:23:02 +0500

    loop & input (loop_data types, while_loop, controls)

---
+++ Class 08 loop & input/class08.py
@@ -0,0 +1,11 @@
+# name : str = input("what is your name : ")
+
+# print(type(name))
+# print(f"Welcome Dear User Mr/Miss {name}!")
+
+import sys
+
+print("line1")
+print("line2")
+
+print(sys.argv)
